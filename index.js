38b66673cd9c4d1ef461e4dd702ecd0d3731dac0
Dan Muchiri <dan.muchiri@student.moringaschool.com>
2023-12-11 14:48:37 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+let messages =[];
+function writeCards(names, event) {
+    for (let i = 0; i < names.length; i++) {
+ let message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+ messages.push(message);
+}
+return messages;
+  }
+
+
+function countDown(n){
+    console.log(n);
+    while (n > 0) {
+        n--;
+        console.log(n);
+      }
+}
